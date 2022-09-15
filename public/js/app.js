9cc03a58a57d4a1ffbaccf1c7d31675032ac01d9
Syed Rafay <syedrafay76432@gmail.com>
2022-09-15 20:25:08 +0500

    readd

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,8 @@
 console.log("client site javascript is loaded")
-
 const weatherForm =  document.querySelector('form')
 const search = document.querySelector('input')
 const message_1 = document.querySelector('#message_1')
 const message_2 = document.querySelector('#message_2')
-
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
